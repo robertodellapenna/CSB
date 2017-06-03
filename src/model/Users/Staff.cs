ee953ae137c2f94b4eff3ea5dee2e5deb2ffc8d6
loreProgetto <lorenzo@antoninizorzi.it>
2017-06-03 18:04:07 +0200

    Inserimento classe User e Client

---
+++ src/model/Users/Staff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSP_Project.src.model.Users;
+
+namespace CSP_Project.src.model.Users
+{
+    public class Staff : User
+    {
+        #region Eventi
+        #endregion
+        #region Campi
+        private int _levelOfAuthorization;
+        #endregion
+        #region Proprieta
+        public int LevelOfAuthorization { get => _levelOfAuthorization; set => _levelOfAuthorization = value; }
+        #endregion
+        #region Costruttori
+        public Staff(string firstName, string lastName, string username, string password, int levelOfAuthorization)
+        : base(firstName, lastName, username, password)
+        {
+            if (levelOfAuthorization < 0)
+                throw new ArgumentException("level of authorization inconsistent");
+            _levelOfAuthorization = levelOfAuthorization;
+        }
+        #endregion
+        #region Metodi
+        #endregion
+        #region Handler
+        #endregion
+
+    }
+
+
+}
---
+++ test/model/Users/StaffAndClientTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSP_Project.src.model.Users;
+
+namespace test.model
+{
+    [TestClass]
+    class StaffAndClientTest
+    {
+        [TestMethod]
+        public void StuffTest()
+        {
+            string nome = "Lorenzo";
+            Staff staffOk = new Staff("Lorenzo", "Antonini", "pippo95", "pluto", 1);
+            Staff staffError = new Staff("", "Antonini", "pippo95", "pluto", 1);
+            Assert.AreEqual(staffOk.FirstName, nome);
+            Assert.ThrowsException<ArgumentException>(() => new Staff("", "Antonini", "pippo95", "pluto", 1));
+        }
+    }
+}
