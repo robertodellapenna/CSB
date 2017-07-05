8d8a3151c1ae853a5ac62ec9793aa8efe40e73cb
Roberto Della Penna <roberto.dellapenna@studio.unibo.it>
2017-07-05 11:32:31 +0200

    Debug conflitto

---
+++ src/business/StructureCoordinator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSB_Project.src.business
+{
+    public interface IStructureCoordinator : ICoordinator
+    {
+
+    }
+
+    public class StructureCoordinator : AbstractCoordinatorDecorator, IStructureCoordinator
+    {
+        #region Eventi
+        #endregion
+
+        #region Campi
+        #endregion
+
+        #region Proprietà
+        #endregion
+
+        #region Costruttori
+        public StructureCoordinator(ICoordinator next) : base(next)
+        {
+        }
+        #endregion
+
+        #region Metodi
+
+        #endregion
+
+        #region Handler
+        #endregion
+
+    }
+}
