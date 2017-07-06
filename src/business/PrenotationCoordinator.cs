778b87834c430630a454a5770131d7765c3670e9
Roberto Della Penna <roberto.dellapenna@studio.unibo.it>
2017-07-06 20:52:22 +0200

    Update PrenotationCoordinator -aggiunto IsAvailable(sector, position, rangeData) -modificati nomi metodi : IsValid-->CanAdd , Filter -->BusyPosictions -reso privato altro metodo Filter-->BookedItems

--- src/business/PrenotationCoordinator.cs
+++ src/business/PrenotationCoordinator.cs
@@ -14,10 +14,10 @@ namespace CSB_Project.src.business
         IEnumerable<Prenotation> Prenotations { get; }
         void AddPrenotation(Prenotation prenotation);
         void AddItemPrenotation(int idPrenotation, ItemPrenotation itemPrenotation);
-        IEnumerable<IBookableItem> Filter(DateRange rangeData);
-        IEnumerable<Position> Filter(Sector sector, DateRange rangeData);
-        bool IsValid(ItemPrenotation itemPrenotation);
-        bool IsValid(Prenotation prenotation);
+        IEnumerable<Position> BusyPositions(Sector sector, DateRange rangeData);
+        bool IsAvailable(Sector sector,Position position, DateRange rangeData);
+        bool CanAdd(ItemPrenotation itemPrenotation);
+        bool CanAdd(Prenotation prenotation);
         event EventHandler PrenotationChanged;
     }
 
@@ -69,7 +69,7 @@ namespace CSB_Project.src.business
             #region Precondizioni
             if (prenotation == null)
                 throw new ArgumentNullException("prenotation null");
-            if (!IsValid(prenotation))
+            if (!CanAdd(prenotation))
                 throw new Exception("prenotation not valid");
             #endregion
             _prenotations.Add(prenotation);
@@ -83,25 +83,12 @@ namespace CSB_Project.src.business
                 throw new ArgumentNullException("item prenotation null");
             if (GetPrenotation(idPrenotation)==null)
                 throw new Exception("prenotation not found");
-            if(!IsValid(itemPrenotation))
+            if(!CanAdd(itemPrenotation))
                 throw new Exception("item prenotation not valid");
             #endregion
             GetPrenotation(idPrenotation).AddItem(itemPrenotation);
         }
-        public IEnumerable<IBookableItem> Filter(DateRange rangeData)
-        {
-            #region Precondizioni
-            if (rangeData == null)
-                throw new ArgumentNullException("rangeData null");
-            #endregion
-            List<IBookableItem> result = new List<IBookableItem>();
-            foreach (Prenotation p in _prenotations)
-                foreach (ItemPrenotation item in p.Items)
-                    if (item.RangeData.OverlapWith(rangeData))
-                        result.Add(item.BookableItem);
-            return result;
-        }
-        public IEnumerable<Position> Filter(Sector sector, DateRange rangeData)
+        public IEnumerable<Position> BusyPositions(Sector sector, DateRange rangeData)
         {
             #region Precondizioni
             if (sector == null)
@@ -109,11 +96,26 @@ namespace CSB_Project.src.business
             if (rangeData == null)
                 throw new ArgumentNullException("rangeData null");
             #endregion
-            return (from item in Filter(rangeData)
+            return (from item in BookedItems(rangeData)
                     where item.Sector == sector
                     select item.Position);
         }
-        public bool IsValid(ItemPrenotation itemPrenotation)
+        public bool IsAvailable(Sector sector, Position position, DateRange rangeData)
+        {
+            #region Precondizioni
+            if (sector == null)
+                throw new ArgumentNullException("sector null");
+            if (position == null)
+                throw new ArgumentNullException("sector null");
+            if (rangeData == null)
+                throw new ArgumentNullException("rangeData null");
+            if (position.Row > sector.Rows || position.Column > sector.Columns)
+                throw new Exception("invalid position for this sector");
+            #endregion
+            return !BusyPositions(sector, rangeData).Where(pos => pos.Row == position.Row &&
+                                                                 pos.Column == position.Column).Any();
+        }
+        public bool CanAdd(ItemPrenotation itemPrenotation)
         {
             #region Precondizioni
             if (itemPrenotation == null)
@@ -128,15 +130,28 @@ namespace CSB_Project.src.business
                 ).Any()
             ).Any();
         }
-        public bool IsValid(Prenotation prenotation)
+        public bool CanAdd(Prenotation prenotation)
         {
             #region Precondizioni
             if (prenotation == null)
                 throw new ArgumentNullException("prenotation null");
             #endregion
             foreach (ItemPrenotation item in prenotation.Items)
-                if (!IsValid(item)) return false;
+                if (!CanAdd(item)) return false;
             return true;
+        }
+        private IEnumerable<IBookableItem> BookedItems(DateRange rangeData)
+        {
+            #region Precondizioni
+            if (rangeData == null)
+                throw new ArgumentNullException("rangeData null");
+            #endregion
+            List<IBookableItem> result = new List<IBookableItem>();
+            foreach (Prenotation p in _prenotations)
+                foreach (ItemPrenotation item in p.Items)
+                    if (item.RangeData.OverlapWith(rangeData))
+                        result.Add(item.BookableItem);
+            return result;
         }
         private Prenotation GetPrenotation(int idPrenotation)
         {
