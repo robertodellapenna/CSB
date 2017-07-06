8640e3e55caf77458951d40a067b062302584a31
loreProgetto <lorenzo@antoninizorzi.it>
2017-07-06 16:56:55 +0200

    Sistemzione Bunble e init ServiceCoordinator

--- src/model/Services/Bundle.cs
+++ src/model/Services/Bundle.cs
@@ -12,30 +12,32 @@ namespace CSB_Project.src.model.Services
         #endregion
         #region Campi
         private readonly ISet<IPacket> _packets;
-        private readonly double _price;
-        private readonly DateRange _availability;
-        private readonly string _name;
-        private readonly string _description;
+        private readonly DatePriceDescriptor _descriptor;
         #endregion
         #region Proprieta
         public ISet<IPacket> Packets => _packets;
 
-        public double Price => _price;
+        public double Price => _descriptor.Price;
 
-        public DateRange Availability => _availability;
+        public DateRange Availability => _descriptor.Range;
 
-        public string Name => _name;
+        public string Name => _descriptor.Name;
 
-        public string Description => _description;
+        public string Description => _descriptor.Description;
         #endregion
         #region Costruttori
-        public Bundle(ISet<IPacket> packets, double price, DateRange availability, string name, string description)
+        
+        public Bundle(ISet<IPacket> packets, DatePriceDescriptor descriptor)
         {
+            if (packets == null || packets.Count == 0)
+                throw new ArgumentException("packets not valid");
+            foreach (IPacket packet in packets)
+                if (!packet.Availability.Contains(descriptor.Range))
+                    throw new ArgumentException("packet range not valid" + packet.Name);
             _packets = packets;
-            _price = price;
-            _availability = availability;
-            _name = name;
-            _description = description;
+            if (descriptor == null)
+                throw new ArgumentException("descriptor not valid");
+            _descriptor = descriptor;
         }
         #endregion
         #region Metodi
